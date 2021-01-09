c1837d6789464577bbea1f7c2aba75d41dc212f3
Fredrik Welander <fredrik.welander@subsite.fi>
2021-01-09 20:49:55 +0200

    Lektion 1

--- src/main/java/com/assignment/Main.java
+++ src/main/java/com/assignment/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		Player player = new Player();
+
+		System.out.println("Hello");
+
 	}
 }
 
---
+++ src/main/java/com/assignment/Player.java
@@ -0,0 +1,11 @@
+package com.assignment;
+
+public class Player {
+
+    private String name = "Fredrik";
+
+    public Player() {
+
+    }
+
+}
