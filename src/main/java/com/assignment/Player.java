e6c7eb87d3694cc54671a07a96c3344e5d902ddc
Fredrik Welander <fredrik.welander@subsite.fi>
2021-01-12 15:08:59 +0200

    Lektion 1, Uppg1-exempel

--- src/main/java/com/assignment/Player.java
+++ src/main/java/com/assignment/Player.java
@@ -2,10 +2,40 @@ package com.assignment;
 
 public class Player {
 
-    private String name = "Fredrik";
+    // åtkomst datatyp variabelnamn = "värde"
+    private String name;
+    private double money = 100.0;
+    // https://en.wikipedia.org/wiki/Floating-point_arithmetic#Accuracy_problems
+    static int playerCounter = 0;
 
-    public Player() {
+    /**
+     * Konstruktormetoden körs när klassen instansieras och har alltid
+     * samma namn (med stor begynnelsebokstav) som klassen
+     */
+    public Player(String name) {
+        setName(name);
+        playerCounter++;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+    public boolean isBroke() {
+        /*boolean ret = false;
+        if (money <= 0) {
+            ret = true;
+        }
+        return ret;*/
+        return money <= 0;
     }
 
 }
